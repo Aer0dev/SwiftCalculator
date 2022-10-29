7468ef5a53965cd3f329ecf561152efb8c98cdd1
Mana_Potion <45550827+Aer0dev@users.noreply.github.com>
2022-10-29 21:13:55 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sh on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
